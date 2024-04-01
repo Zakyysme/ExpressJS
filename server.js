2fc249830931ec177c63c7d280489404e27cc07b
Zakyysme <146419282+Zakyysme@users.noreply.github.com>
2024-04-01 13:05:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 app.use((express.json()))
 const PORT = 3211 || process.env.PORT
 
-const SUPABASE_URL = "https://twkqtivsuyffancyevsl.supabase.co";
+const SUPABASE_URL = "";
 const SUPABASE_SERVICE_ROLE =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3a3F0aXZzdXlmZmFuY3lldnNsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTgzMzUwNCwiZXhwIjoyMDI3NDA5NTA0fQ.jOW0BWL8OK0zIu0dclnwNKw0vv0zLRQ5ZPas9CMM0e8";
-
+  "";
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE);
 
 app.get("/", async(request, response) => {
